d585f8f881a2c69f732aac2d4f863b1b379fcfba
dezroge <desmondrogers2@gmail.com>
2019-06-15 00:14:14 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+Some additional requirements for your application:
+
+- The bill value must be a positive float but your application must
+  handle user input that might be a negative or 0 dollar amount
+  (i.e. error handling).
+- The user input percentage for the tip must be a positive float.
+- The returned value must be a float rounded to two decimal points
+  (i.e. accurate dollars and cents).
+*/
